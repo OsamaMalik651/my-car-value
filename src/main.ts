55451d2188401651d01da1bfee3c748a486b6c22
OsamaMalik651 <osama.malik013@gmai.com>
2022-10-10 20:04:14 -0600

    setup user functionality created controllers and related files.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //makes sure that the incoming requests don't have extraneous properties.
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
